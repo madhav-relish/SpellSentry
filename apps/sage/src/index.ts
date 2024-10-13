48d6fba503ea72ee63d8407b3bb2867f53c39bac
sajdkabir <sajda.kbir@gmail.com>
2024-10-13 21:28:52 +0530

    server is up

--- apps/sage/src/index.ts
+++ apps/sage/src/index.ts
@@ -3,13 +3,17 @@ import dotenv from 'dotenv';
 import bodyParser from 'body-parser';
 import aiAnalysis from './routes/openAiAnalysis';
 
-// Load environment variables
+
 dotenv.config();
 
 const app: Application = express();
 const port = process.env.PORT || 9001;
 
 app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("SpellSenty backend API is up and running!");
+  });
 
 
 app.use('/api', aiAnalysis);
